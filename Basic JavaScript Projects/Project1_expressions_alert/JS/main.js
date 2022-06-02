b1afa96dc33c1c9bc1315e3a0b9078a4a44ca73e
nubiajadebrice <105795558+nubiajadebrice@users.noreply.github.com>
2022-06-02 11:08:27 -0500

    basic javascript assignment
    worked on basic introductory JS coding like alerts, variables, expressions, etc...

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,25 @@
+window.alert("Hello, World!") //creates a pop up alert
+
+document.write("Hello, World!") //creates a pop up alert
+
+var A = " This is a string.";
+document.write(A); //displayed the variable
+
+var B = " String Assignment."
+window.alert(B) //displayed the variable
+
+document.write(" Shakespeare said, \"To be, or not to be.\"") 
+//used backslash to show characters
+
+document.write(" A quote: "
++ "\"I don\'t care what you think about me,"
++ " I don\'t think about you at all.\""
++" -Coco Chanel") //used + to connect strings
+
+var C = " And if I ever" + " fall in love again"
+document.write(C) //connected strings using a variable
+
+var family = "Jayson Lane", Dad = "Brice", Mom = "Delia", Grandson = "Tatum";
+document.write (Dad); //created multiple values for this variable and displayed one
+
+document.write(3+3); //this is a statement and expression that will display the result of the expression
