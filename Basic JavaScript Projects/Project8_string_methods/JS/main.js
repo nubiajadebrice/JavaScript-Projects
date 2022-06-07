986902c11bf86d7d05562897601139e870bc3e97
nubiajadebrice <105795558+nubiajadebrice@users.noreply.github.com>
2022-06-07 10:45:19 -0500

    string methods
    utilized different string methods including concat, slice, tostring, toprecision, tofixed, and valueof

--- Basic JavaScript Projects/Project8_string_methods/JS/main.js
+++ Basic JavaScript Projects/Project8_string_methods/JS/main.js
@@ -20,5 +20,25 @@ const str2 = "Nubia likes the color yellow.";
 document.write(str2.search("yellow"));
 //searches the string and returns the value of yellow
 
+function string_method() {
+    var X = 21;
+    document.getElementById("numb_to_string").innerHTML = X.toString();
+} //this converts a number to a string and displays that
+
+function precision_method() {
+    var X = 2116.123456789987654321
+    document.getElementById("precision").innerHTML = X.toPrecision(7);
+} //this will only display the number up to the amount of characters specify
+
+function fix_method() {
+    var X = 2116.123456789
+    document.getElementById("fixing").innerHTML = X.toFixed(3);
+} //this will only display the number up to the amount of decimals that I specify
+
+
+const Y = "Nubia says hi.";
+console.log(Y.valueOf());
+//this will display the value of my string in the console
+
 
 
