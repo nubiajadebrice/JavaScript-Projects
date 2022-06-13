55345eed4a94543b67162abe1050f7de45ef52c7
nubiajadebrice <105795558+nubiajadebrice@users.noreply.github.com>
2022-06-13 13:39:34 -0500

    pizza edits
    added more detail to the pizza menu

--- Basic JavaScript Projects/Pizza_Project/Pizza_JavaScript/Pizza.js
+++ Basic JavaScript Projects/Pizza_Project/Pizza_JavaScript/Pizza.js
@@ -47,8 +47,14 @@ function getTopping(runningTotal,text1) {
     } else {
         toppingTotal = 0;
     }
-    runningTotal
-}
+    runningTotal = (runningTotal + toppingTotal);
+    console.log("total selected topping items: "+toppingCount);
+    console.log(toppingCount+" topping - 1 free topping = "+"$"+toppingTotal+".00");
+    console.log("topping text1: "+text1);
+    console.log("Purchase Total: "+"$"+runningTotal+".00");
+    document.getElementById("showText").innerHTML=text1;
+    document.getElementById("totalPrice").innerHTML = "<h3>Total: <strong>$"+runningTotal+".00"+"</strong></h3>";
+};
 
 
 
